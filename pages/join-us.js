3e8416c8e736cfa3e8fa96327799a781bbd5a1ee
nidhamu <nidhamu2d3@gmail.com>
2025-03-13 16:55:09 +0000

    トップページのスタイルに移植完了

---
+++ pages/join-us.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import config from "@config/config.json"
+import Base from "@layouts/Baseof"
+import Image from "next/image"
+import Link from "next/link"
+import { getListPage } from "../lib/contentParser"
+
+import { BannerSection } from "@content/components/sections/join-us/BannerSection"
+import { EntryProcedureSection } from "@content/components/sections/join-us/EntryProcedureSection"
+
+
+const Home = ({ frontmatter }) => {
+  const { title } = config.site;
+
+  // 言語の状態を管理（デフォルトは "ja"）
+  const [language, setLanguage] = useState("ja");
+
+  return (
+    <div>
+      <Base title={title}>
+        <BannerSection />
+
+        {/* 言語切り替えタブ */}
+        <div className="flex justify-end space-x-4 bg-black">
+          <button
+            className={`px-4 py-2 border-b-2 ${language === "ja" ? "border-blue-500 font-bold text-white" : "border-transparent text-gray-300"
+              }`}
+            onClick={() => setLanguage("ja")}
+          >
+            日本語
+          </button>
+          <button
+            className={`px-4 py-2 border-b-2 ${language === "en" ? "border-blue-500 font-bold text-white" : "border-transparent text-gray-300"
+              }`}
+            onClick={() => setLanguage("en")}
+          >
+            English
+          </button>
+        </div>
+
+        {/* 言語ごとのコンテンツ切り替え */}
+        {language === "ja" ? (
+          <>
+            <EntryProcedureSection language="ja" />
+          </>
+        ) : (
+          <>
+            <EntryProcedureSection language="en" />
+          </>
+        )}
+      </Base>
+    </div>
+  );
+};
+
+export default Home;
