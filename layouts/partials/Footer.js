f781cd6c7d798cd23d4298310622e3f269589347
DDlia <gh@ddlia.com>
2026-06-21 18:11:40 +0900

    style: make footer layout responsive on mobile devices

--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -3,8 +3,7 @@ import config from "@config/config.json";
 import menu from "@config/menu.json";
 import social from "@config/social.json";
 import { markdownify } from "@lib/utils/textConverter";
-import Image from "next/image";
-import Link from "next/link";
+import Logo from "@components/Logo";
 
 const Footer = () => {
   const { copyright, footer_content } = config.params;
@@ -16,7 +15,7 @@ const Footer = () => {
         <div className="row">
           {footer.map((col) => {
             return (
-              <div className="mb-12 col-6 lg:col-3" key={col.name}>
+              <div className="mb-12 col-12 sm:col-6 lg:col-3" key={col.name}>
                 {markdownify(col.name, "h2", "h4")}
                 <ul className="mt-6">
                   {col?.menu.map((item) => (
@@ -28,16 +27,8 @@ const Footer = () => {
               </div>
             );
           })}
-          <div className="md-12 lg:col-6">
-            <Link href="/" aria-label="TechnoTUT">
-              <Image
-                src="/images/logo/Logo_Bblack.svg"
-                width={config.site.logo_width}
-                height={config.site.logo_height}
-                alt=""
-                className="w-auto h-auto"
-              />
-            </Link>
+          <div className="col-12 lg:col-6">
+            <Logo src="/images/logo/Logo_Bblack.svg" />
             {markdownify(footer_content, "p", "")}
           </div>
         </div>
